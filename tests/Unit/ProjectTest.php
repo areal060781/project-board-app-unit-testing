b0cab492333810da690342bb11aea92f2ca85baa
Aida Real <areal060781@gmail.com>
2020-08-11 13:38:55 +0000

    Model test

---
+++ tests/Unit/ProjectTest.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace Tests\Unit;
+
+use App\Project;
+use Illuminate\Foundation\Testing\RefreshDatabase;
+//use PHPUnit\Framework\TestCase;
+use Tests\TestCase;
+
+class ProjectTest extends TestCase
+{
+    use RefreshDatabase;
+
+    /** @test */
+    public function it_has_a_path()
+    {
+        $project = factory(Project::class)->create();
+
+        $this->assertEquals('/projects/' . $project->id, $project->path());
+    }
+
+
+}
