3f2e8775fe60d606c9ce13c7fbfcab9084c2cd67
Aida Real <areal060781@gmail.com>
2020-08-12 22:46:17 +0000

    Set up Tailwindcss

--- public/js/app.js
+++ public/js/app.js
@@ -37331,8 +37331,8 @@ window.axios.defaults.headers.common['X-Requested-With'] = 'XMLHttpRequest';
 /*! no static exports found */
 /***/ (function(module, exports, __webpack_require__) {
 
-__webpack_require__(/*! /home/vagrant/birdboard/resources/js/app.js */"./resources/js/app.js");
-module.exports = __webpack_require__(/*! /home/vagrant/birdboard/resources/sass/app.scss */"./resources/sass/app.scss");
+__webpack_require__(/*! /home/aida/PHP/laravel/birdboard/resources/js/app.js */"./resources/js/app.js");
+module.exports = __webpack_require__(/*! /home/aida/PHP/laravel/birdboard/resources/sass/app.scss */"./resources/sass/app.scss");
 
 
 /***/ })
