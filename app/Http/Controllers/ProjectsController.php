5173d9889070ced5867fc13e038626e6a0421859
Aida Real <areal060781@gmail.com>
2020-08-19 22:55:29 +0000

    Delete projects

--- app/Http/Controllers/ProjectsController.php
+++ app/Http/Controllers/ProjectsController.php
@@ -95,4 +95,13 @@ class ProjectsController extends Controller
 
         return redirect($project->path());
     }
+
+    public function destroy(Project $project)
+    {
+        $this->authorize('update', $project);
+
+        $project->delete();
+
+        return redirect('/projects');
+    }
 }
--- app/RecordsActivity.php
+++ app/RecordsActivity.php
@@ -50,7 +50,7 @@ trait RecordsActivity
             return static::$recordableEvents;
         }
 
-        return ['created', 'updated', 'deleted'];
+        return ['created', 'updated'];
     }
 
     /**
